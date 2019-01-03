3faa9634d48063f0a5cd6fe851fdad8c3914b42a
aryan katebian <aryankatebian@aryans-MacBook-Pro.local>
2019-01-03 19:53:36 +0000

    landingPage url changes

--- src/Main.js
+++ src/Main.js
@@ -8,7 +8,7 @@ import LandingPage from "./Components/LandingPage/LandigPage";
 
 const Main = () => (
   <Switch>
-    <Route exact path="/" component={LandingPage} />
+    <Route exact path="/portfolio" component={LandingPage} />
     <Route path="/aboutme" component={AboutMe} />
     <Route path="/skills" component={Skills} />
     <Route path="/projects" component={Projects} />

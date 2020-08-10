4a5a60d1eca3c77c94bd18b7d4b5f30031e38cdb
aryan <aryan@aryans-MacBook-Pro.local>
2020-08-10 15:59:52 +0100

    Updates

---
+++ precache-manifest.ce3f30e574374745de607859dc9f44e5.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = [
+  {
+    "revision": "56ef42a9548c69a7b0a6",
+    "url": "/portfolio/static/css/main.b0e1d477.chunk.css"
+  },
+  {
+    "revision": "56ef42a9548c69a7b0a6",
+    "url": "/portfolio/static/js/main.56ef42a9.chunk.js"
+  },
+  {
+    "revision": "1a1f463e4db6c0740257",
+    "url": "/portfolio/static/js/1.1a1f463e.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "506f2718ca391c42b9916eea2e511b99",
+    "url": "/portfolio/static/media/js.506f2718.png"
+  },
+  {
+    "revision": "2ea01c0e92883b6e481dd2e62f5e6d4e",
+    "url": "/portfolio/static/media/html.2ea01c0e.png"
+  },
+  {
+    "revision": "9fe7584b6d29bec74a66024829929697",
+    "url": "/portfolio/static/media/css.9fe7584b.png"
+  },
+  {
+    "revision": "8db6417568a6d44799372fac25e4f592",
+    "url": "/portfolio/static/media/wordpress.8db64175.png"
+  },
+  {
+    "revision": "689c4e1f9ad9a8e685933125980a1b31",
+    "url": "/portfolio/static/media/icon3.689c4e1f.png"
+  },
+  {
+    "revision": "36700bee9a1831f2691165d291d62d78",
+    "url": "/portfolio/static/media/node.36700bee.png"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/portfolio/static/media/react.47ce6e77.png"
+  },
+  {
+    "revision": "0897462cd2a704ab16e9d89104779ff5",
+    "url": "/portfolio/static/media/ground.0897462c.png"
+  },
+  {
+    "revision": "8787196d69fae14c69be0076913be4ab",
+    "url": "/portfolio/static/media/holder.8787196d.png"
+  },
+  {
+    "revision": "85ba9dff7e4f219114b75eac2630d660",
+    "url": "/portfolio/static/media/rocket.85ba9dff.png"
+  },
+  {
+    "revision": "a2a3bc7c0349afddcd42f436afe4fc47",
+    "url": "/portfolio/static/media/url-shortener.a2a3bc7c.png"
+  },
+  {
+    "revision": "8251fe76d7487a28b07603515b300964",
+    "url": "/portfolio/static/media/redux.8251fe76.png"
+  },
+  {
+    "revision": "2b7c9230f09becf834193c134c15867c",
+    "url": "/portfolio/static/media/alien3.2b7c9230.png"
+  },
+  {
+    "revision": "cd27333b6b4d6351bad2d5c854aca6b7",
+    "url": "/portfolio/static/media/stack.cd27333b.jpg"
+  },
+  {
+    "revision": "9d4e7c4d633644b2147cf84a6c112a94",
+    "url": "/portfolio/static/media/m1.9d4e7c4d.jpg"
+  },
+  {
+    "revision": "3970d8a9c831f9ab83bfa30d9d9d4659",
+    "url": "/portfolio/static/media/m2.3970d8a9.jpg"
+  },
+  {
+    "revision": "1c63c3a53b009f6f43e0d9cecd7a47ea",
+    "url": "/portfolio/static/media/m3.1c63c3a5.jpg"
+  },
+  {
+    "revision": "b2b3c5135dfb7d1ced6f659cab4a0dab",
+    "url": "/portfolio/static/media/m4.b2b3c513.jpg"
+  },
+  {
+    "revision": "6045fadebbb0279b165d68dfd730f111",
+    "url": "/portfolio/static/media/me.6045fade.jpg"
+  },
+  {
+    "revision": "8b60da5513474e21039a8217c191a60e",
+    "url": "/portfolio/static/media/landingpage.8b60da55.jpg"
+  },
+  {
+    "revision": "e57058c07d9a912afdd93b4f3de88ed5",
+    "url": "/portfolio/static/media/vue.e57058c0.jpg"
+  },
+  {
+    "revision": "98459dc0e033829ab21cca59d2c624a4",
+    "url": "/portfolio/static/media/weather.98459dc0.png"
+  },
+  {
+    "revision": "eea938de1b129eb25c0697cb371ac987",
+    "url": "/portfolio/static/media/videoplayer.eea938de.png"
+  },
+  {
+    "revision": "555a9cdde1ce60de4e4525117b6bf1d0",
+    "url": "/portfolio/static/media/stickynav.555a9cdd.png"
+  },
+  {
+    "revision": "ffb1cea044ffba5a9a6980b34bf42936",
+    "url": "/portfolio/static/media/drum.ffb1cea0.png"
+  },
+  {
+    "revision": "6498ad3d8579c0cd891e9f705b50d50d",
+    "url": "/portfolio/static/media/game.6498ad3d.png"
+  },
+  {
+    "revision": "5074d0d9b63f7904d63ac9e3fd59cd06",
+    "url": "/portfolio/static/media/nerd.5074d0d9.png"
+  },
+  {
+    "revision": "2b902b3718050ac32792cc82331431f5",
+    "url": "/portfolio/static/media/mfs.2b902b37.png"
+  },
+  {
+    "revision": "45f19f3abc5022a5996b09aa9d3de825",
+    "url": "/portfolio/static/media/numeraldesign.45f19f3a.png"
+  },
+  {
+    "revision": "8292067f03989fde76a1467fad503cfc",
+    "url": "/portfolio/static/media/ashori.8292067f.png"
+  },
+  {
+    "revision": "02259a8bdaaf427050055a5790c7aad5",
+    "url": "/portfolio/static/media/elika.02259a8b.png"
+  },
+  {
+    "revision": "edad93a4db5a82c07d58ec7eb8037a19",
+    "url": "/portfolio/static/media/body.edad93a4.png"
+  },
+  {
+    "revision": "644db5dd1fce3673712678b90898e8eb",
+    "url": "/portfolio/static/media/store.644db5dd.png"
+  },
+  {
+    "revision": "52f39191b94b7e737bca21056cec8f28",
+    "url": "/portfolio/static/media/e-comm.52f39191.png"
+  },
+  {
+    "revision": "107a2f247224d0d70d92f06965975d26",
+    "url": "/portfolio/static/media/Ninety-Seven.107a2f24.otf"
+  },
+  {
+    "revision": "72eecb4017b38dc611647ea8b47a44bb",
+    "url": "/portfolio/static/media/io.72eecb40.png"
+  },
+  {
+    "revision": "6fe5a09bafdaca67a8d195077a0f2c02",
+    "url": "/portfolio/static/media/earth.6fe5a09b.png"
+  },
+  {
+    "revision": "1bd06f7ec8c86d0b70007f7302ac5b72",
+    "url": "/portfolio/static/media/moon.1bd06f7e.png"
+  },
+  {
+    "revision": "5976f73d9b9abe99b04f5020c30d87db",
+    "url": "/portfolio/static/media/jupiter.5976f73d.png"
+  },
+  {
+    "revision": "f121b82548d6545f618ccb51c4cf63b9",
+    "url": "/portfolio/index.html"
+  }
+];
